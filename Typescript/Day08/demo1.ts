00207df6e5a835584b600a3ba9bc97a8d177d657
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-11-17 21:42:30 +0530

    Day08

---
+++ Typescript/Day08/demo1.ts
@@ -0,0 +1,35 @@
+ export class Myclass{
+   private id:number;
+   public fname:string;
+   protected lname:string;
+
+   constructor(id:number,f:string,l:string){
+        this.id=id;
+        this.fname=f;
+        this.lname=l;
+   }
+
+    // constructor(){
+
+    // }
+
+  
+   
+  display(){
+    console.log(`
+        Id        :: ${this.id}
+        First name::${this.fname}
+        Last name ::${this.lname}
+    `)
+   }
+
+}
+
+// let obj=new Myclass(9,'Sumit','Raokhande');
+// obj.display();
+
+export function add(a:number,b:number):number{
+    return (a+b);
+}
+
+export const pi=3.14;
---
+++ Typescript/Day08/demo2.ts
@@ -0,0 +1,11 @@
+
+import {Myclass,add,pi} from './demo1';
+
+let obj=new Myclass(3,'Spruha','Raokhande');
+obj.display();
+
+console.log(`
+    Addition :: ${add(10,4)}
+`)
+
+console.log("Pi is "+pi);
