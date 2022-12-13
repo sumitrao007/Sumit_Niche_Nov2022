36700451d488d3850cb9351a7d671c379f357e06
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-12-13 21:37:51 +0530

    Day21

---
+++ Angular/Day21/course.service.ts
@@ -0,0 +1,10 @@
+
+export class CourseService{
+
+    private course:string[]=['Angular 12','React Js','Vue Js','Embber Js','Knowout Js'];
+
+    getCourse(){
+        return this.course;
+    }
+
+}
