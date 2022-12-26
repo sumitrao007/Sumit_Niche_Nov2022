9944d7bf86b7e7da7fa223bcdb723f018619668f
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-12-26 21:44:27 +0530

    Day29

---
+++ Angular/Day29/demotdf/demotdf.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demotdf',
+  templateUrl: './demotdf.component.html',
+  styleUrls: ['./demotdf.component.css']
+})
+export class DemotdfComponent implements OnInit {
+
+  myname:string='admin';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
